3c804989b085bfeed0fce55a4db774a2acde16e2
michalskibinski109 <michalskibinski109@gmail.com>
2023-07-05 00:02:19 +0200

    Add requirements

--- setup.py
+++ setup.py
@@ -1,17 +1,11 @@
 import setuptools
 
 
-LONG_DESCRIPTION = """
-
-"""
-
 setuptools.setup(
     name="fast-checkers",
     version="0.0.1",
     author="Michał Skibiński",
     author_email="mskibinski109@gmail.com",
-    long_description_content_type="text/markdown",
-    long_description=LONG_DESCRIPTION,
     files_to_include=["checkers"],
     packages=setuptools.find_packages(),
     classifiers=[
