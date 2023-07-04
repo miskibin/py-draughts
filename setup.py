04a962f6f2d988448d8699c56c4b2e2126e0f242
Michał Skibiński <77834536+michalskibinski109@users.noreply.github.com>
2023-07-05 00:17:28 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,16 +3,17 @@ import setuptools
 
 setuptools.setup(
     name="fast-checkers",
-    version="0.0.1",
+    version="0.0.2",
     author="Michał Skibiński",
     author_email="mskibinski109@gmail.com",
     files_to_include=["checkers"],
-    long_description="Modern checkers library with fast move generation. Still in development.",
+    long_description="Modern checkers library with fast move generation. Still under development.",
     packages=setuptools.find_packages(),
+    url="https://github.com/michalskibinski109/checkers",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.10",
 )
