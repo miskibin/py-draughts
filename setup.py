bc1918c151217b0b5581c40e2062be15acd268ce
Skibinski Michal <michal.skibinski@nokia.com>
2023-07-05 13:47:39 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -3,14 +3,10 @@ from checkers.__init__ import __doc__
 import os
 
 
-def read_description():
-    """
-    Reads the description from README.rst and substitutes mentions of the
-    latest version with a concrete version number.
-    """
-    with open("readme.md", encoding="utf-8") as f:
-        description = f.read()
-    return description
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setuptools.setup(
     name="fast-checkers",
     version="0.0.3",
@@ -18,7 +14,7 @@ setuptools.setup(
     author_email="mskibinski109@gmail.com",
     files_to_include=["checkers"],
     description=__doc__.replace("\n", " ").strip(),
-    long_description=read_description(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     url="https://github.com/michalskibinski109/checkers",
