288586a3009d3503a4f02a0a733b1a7196383adc
michalskibinski109 <michalskibinski109@gmail.com>
2023-07-06 18:45:56 +0200

    Update tests

--- checkers/american.py
+++ checkers/american.py
@@ -17,14 +17,14 @@ from checkers.utils import logger
 """
 
 # fmt: off
-SQUARES = [_, B8, D8, F8, H8,
+SQUARES = [B8, D8, F8, H8,
         A7, C7, E7, G7,
         B6, D6, F6, H6,
         A5, C5, E5, G5,
         B4, D4, F4, H4,
         A3, C3, E3, G3,
         B2, D2, F2, H2,
-        A1, C1, E1, G1] = range(33)
+        A1, C1, E1, G1] = range(32)
 # fmt: on
 
 
---
+++ test/test_american_board.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pytest
+
+import checkers.american as checkers
+from checkers.american import Board, Move, Color
+from checkers.models import Entity
+
+
+class TestAmericanBoard:
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        self.board = Board()
+        yield
+        del self.board
+
+    def test_move_from_str_method(self):
+        legal_moves = self.board.legal_moves
+        m1 = Move.from_string("24-20", legal_moves)
+        assert m1 == Move([checkers.G3, checkers.H4])
+
+        with pytest.raises(ValueError):
+            Move.from_string("25-20", [])
+
+    def test_push_from_string(self):
+        m1 = Move.from_string("24-20", self.board.legal_moves)
+        self.board.push_from_str("24-20")
+        assert self.board.turn == Color.BLACK
+        assert self.board.pop() == m1
+        assert np.array_equal(self.board.position, Board.STARTING_POSITION)
+
+    def test_capture(self):
+        moves = ["24-20", "11-16", "20x11", "7x16"]
+        for m in moves:
+            self.board.push_from_str(m)
+        assert self.board[checkers.F6] == Entity.EMPTY.value
